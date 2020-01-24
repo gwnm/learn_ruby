ed6935079e6f0d6a6a7fe6b90238ca32a5161478
gwnm <gwnmagee@gmail.com>
2020-01-24 16:15:36 +0000

    finished

--- 00_hello/hello.rb
+++ 00_hello/hello.rb
@@ -1,1 +1,10 @@
-#write your code here
+def hello
+     "Hello!"
+     end
+     def greet(alice)
+        "Hello, Alice!"
+      end
+      def greet(name)
+        "Hello, #{name}!"
+     end
+     
